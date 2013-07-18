53799335ba46a4214956ab62a47521481524860b
Mark Redar <mredar@gmail.com>
2013-07-18 10:53:58 -0700

    Moved check_oai_queue to read_sqs_queue

--- oai_harvester/read_sqs_queue.py
+++ oai_harvester/read_sqs_queue.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 '''Check a queue and get times and info for messages in q'''
 import sys
+import os
 import logging
 import json
 import datetime
 import boto.sqs as sqs
-from ucldc_queue import UCLDC_Queues
-#q_names = ('QUEUE_OAI_HARVEST', 'QUEUE_OAI_HARVEST_ERR', 'QUEUE_OAI_HARVEST_HARVESTING')
-#from ucldc_queue import QUEUE_OAI_HARVEST, QUEUE_OAI_HARVEST_ERR, QUEUE_OAI_HARVEST_HARVESTING
+
+QUEUE_OAI_HARVEST = os.environ.get('QUEUE_OAI_HARVEST', 'OAI_harvest')
+QUEUE_OAI_HARVEST_ERR = os.environ.get('QUEUE_OAI_HARVEST_ERR', 'OAI_harvest_error')
+QUEUE_OAI_HARVEST_HARVESTING = os.environ.get('QUEUE_OAI_HARVEST_HARVESTING', 'OAI_harvest_harvesting')
+
+SQS_CONNECTION = sqs.connect_to_region('us-east-1')
+
 
 logging.basicConfig(level=logging.INFO)
 
 def main(args):
     q_name = args[1]
-    queues=UCLDC_Queues()
-    q_harvesting = queues.get_queue(q_name)
+    q_harvesting = SQS_CONNECTION.get_queue(q_name)
     msgs = q_harvesting.get_messages(num_messages=10, visibility_timeout=10, attributes='All') #copied from boto implementation of save_to_file
     while(msgs):
         for m in msgs:
@@ -29,5 +33,5 @@ def main(args):
 
 if __name__=="__main__":
     if len(sys.argv) < 2:
-        print 'Usage: check_oai_queue.py <queue_name>'
+        print 'Usage: read_sqs_queue.py <queue_name>'
     main(sys.argv)
